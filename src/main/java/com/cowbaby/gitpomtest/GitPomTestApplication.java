4d1922d64c36057371d7ca29684694a58c5218b1
admin <1244107044@qq.com>
2024-04-06 11:28:40 +0800

    commit success

---
+++ src/main/java/com/cowbaby/gitpomtest/GitPomTestApplication.java
@@ -0,0 +1,13 @@
+package com.cowbaby.gitpomtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPomTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPomTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cowbaby/gitpomtest/GitPomTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowbaby.gitpomtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPomTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
